aedd3a2f70dac80849bdeaef4945af2c0bd764db
Eldos <eldos.baktybekov@gmail.com>
2024-07-16 14:40:10 +0600

    :wrench: Add primevue config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura'
+
+
 export default defineNuxtConfig({
+  modules: [
+    '@primevue/nuxt-module',
+  ],
+
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+
   devtools: { enabled: true },
   routeRules: {
     // prerender index route by default
