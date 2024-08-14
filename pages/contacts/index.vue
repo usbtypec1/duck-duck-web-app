9415363ed2c1f765b60e69bf0bbcf27c9c3d8676
Eldos <eldos.baktybekov@gmail.com>
2024-08-14 21:04:49 +0600

    :children_crossing: Add avatars for all users

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -4,18 +4,26 @@
       <section v-for="contact in contactsStore.contacts">
         <NuxtLink :to="{ name: 'contacts-id', params: { id: contact.id } }">
           <div class="flex cursor-pointer">
-            <div class="basis-1/4">
+            <div class="basis-1/5">
               <NuxtImg
                 v-if="contact.to_user.profile_photo_url"
                 :src="contact.to_user.profile_photo_url"
-                class="w-full p-2 rounded-full"
+                class="h-16 rounded-full"
                 size="large"
                 placeholder
               />
+              <div
+                v-else
+                :class="[contact.is_hidden ? 'bg-gray-400' : 'bg-emerald-400']"
+                class="w-16 h-16 rounded-full flex justify-center items-center"
+              >
+                <p class="font-semibold text-2xl text-white">{{ contact.private_name.at(0).toUpperCase() }}</p>
+              </div>
             </div>
-            <div>
+            <div class="ml-1">
               <p class="text-md font-semibold">{{ contact.private_name }}</p>
               <p class="text-sm text-gray-600">{{ contact.public_name }}</p>
+              <p v-if="contact.is_hidden" class="text-xs text-gray-500">Скрыт</p>
             </div>
           </div>
         </NuxtLink>
@@ -24,13 +32,16 @@
     </div>
     <p v-else>У вас нет контактов</p>
     <NuxtLink :to="{ name: 'index' }">
-      <Button class="w-full my-3" severity="secondary" outlined label="Назад"/>
+      <MainButton text="Назад"/>
+      <DevOnly>
+        <Button class="w-full my-3" severity="secondary" outlined label="Назад"/>
+      </DevOnly>
     </NuxtLink>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useWebApp } from 'vue-tg'
+import { useWebApp, MainButton } from 'vue-tg'
 
 const { initDataUnsafe } = useWebApp()
 
