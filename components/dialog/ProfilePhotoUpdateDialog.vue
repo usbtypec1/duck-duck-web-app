724e520416f4e3633effa3acc3fa342698dccc08
Eldos <eldos.baktybekov@gmail.com>
2024-09-21 23:07:29 +0600

    :sparkles: Add ability to update profile photo

---
+++ components/dialog/ProfilePhotoUpdateDialog.vue
@@ -0,0 +1,67 @@
+<template>
+  <Dialog v-model:visible="isVisible" modal header="Фото профиля" :style="{ width: '25rem' }">
+    <FileUpload
+      mode="basic"
+      name="profile_photo"
+      :url="url"
+      accept="image/*"
+      :max-file-size="1000000"
+      @upload="onUpload"
+      @error="onError"
+      auto
+      class="w-full"
+      :disabled="uploadStatus !== null"
+      choose-label="Загрузить"
+    />
+    <p
+      v-if="uploadStatus === 'success'"
+      class="text-center mt-1"
+    >
+      Фото успешно загружено
+    </p>
+    <p
+      v-else-if="uploadStatus === 'error'"
+      class="text-center mt-1 text-red-500"
+    >
+      Произошла ошибка
+    </p>
+    <Button
+      type="button"
+      class="w-full mt-3"
+      label="Закрыть"
+      outlined
+      @click="onClose"
+    />
+  </Dialog>
+</template>
+
+<script setup lang="ts">
+import type { FileUploadUploadEvent } from 'primevue/fileupload'
+
+const isVisible = defineModel<boolean>('isVisible')
+
+const uploadStatus = ref<'success' | 'error' | null>(null)
+
+const userStore = useUserStore()
+const runtimeConfig = useRuntimeConfig()
+
+const onClose = () => {
+  isVisible.value = false
+  uploadStatus.value = null
+}
+
+const onUpload = async () => {
+  uploadStatus.value = 'success'
+  await userStore.fetchUser()
+}
+
+const onError = () => {
+  uploadStatus.value = 'error'
+}
+
+const url = computed((): string | undefined => {
+  if (!userStore.userId) return
+  // return `${runtimeConfig.public.apiBaseUrl}/users/${userStore.userId}/profile-photos/`
+  return `http://localhost:8000/users/${userStore.userId}/profile-photos/`
+})
+</script>
--- components/user-profile/UserProfileTelegramInfo.vue
+++ components/user-profile/UserProfileTelegramInfo.vue
@@ -1,12 +1,20 @@
 <template>
   <Fieldset legend="Telegram">
     <div class="flex flex-row items-center gap-x-4">
-      <section class="basis-1/2">
+      <section class="basis-1/2 flex justify-center">
         <NuxtImg
           v-if="user.profile_photo_url"
           :src="user.profile_photo_url"
           alt="profile photo"
-          class="rounded-full aspect-square"
+          class="rounded-full aspect-square cursor-pointer"
+          @click="isProfilePhotoUpdateDialogVisible = true"
+        />
+        <Button
+          v-else
+          label="Добавить фото"
+          text
+          icon="pi pi-image"
+          @click="isProfilePhotoUpdateDialogVisible = true"
         />
       </section>
       <section class="basis-1/2 flex flex-col justify-center">
@@ -23,8 +31,8 @@
         <UserHealthProgressBar :health="user.health"/>
       </section>
     </div>
+    <ProfilePhotoUpdateDialog v-model:is-visible="isProfilePhotoUpdateDialogVisible"/>
   </Fieldset>
-
 </template>
 
 <script setup lang="ts">
@@ -32,8 +40,11 @@ import type { User } from '~/types/users'
 import UserPremium from '~/components/user-profile/UserPremium.vue'
 import UserEnergyProgressBar from '~/components/user-profile/UserEnergyProgressBar.vue'
 import UserHealthProgressBar from '~/components/user-profile/UserHealthProgressBar.vue'
+import ProfilePhotoUpdateDialog from '~/components/dialog/ProfilePhotoUpdateDialog.vue'
 
 const props = defineProps<{
   user: User,
 }>()
+
+const isProfilePhotoUpdateDialogVisible = ref<boolean>(false)
 </script>
